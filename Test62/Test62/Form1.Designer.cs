1ae5ca1297641958058974e124502510c7b11f2f
liubc <liubc@changyi.com>
2018-04-02 16:45:06 +0800

    1

---
+++ Test62/Test62/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Test62
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbOld = new System.Windows.Forms.TextBox();
+            this.tbNew = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(146, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbOld
+            // 
+            this.tbOld.Location = new System.Drawing.Point(12, 12);
+            this.tbOld.Name = "tbOld";
+            this.tbOld.Size = new System.Drawing.Size(128, 21);
+            this.tbOld.TabIndex = 1;
+            // 
+            // tbNew
+            // 
+            this.tbNew.Location = new System.Drawing.Point(12, 48);
+            this.tbNew.Name = "tbNew";
+            this.tbNew.Size = new System.Drawing.Size(128, 21);
+            this.tbNew.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(567, 261);
+            this.Controls.Add(this.tbNew);
+            this.Controls.Add(this.tbOld);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbOld;
+        private System.Windows.Forms.TextBox tbNew;
+    }
+}
+
